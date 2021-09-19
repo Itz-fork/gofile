ed707d7ab95239b09d55ea49d73f7fd7ce821f0f
I'm Not A Bot #Left_TG <77770753+Itz-fork@users.noreply.github.com>
2021-09-19 18:35:29 +0530

    Gofile2

--- setup.py
+++ setup.py
@@ -1,23 +1,47 @@
-from setuptools import setup, find_packages
+# Re-built by Itz-fork
+# Original Author: Codec04
+# Project: Gofile2
+import os
 
-setup(
-    name="gofile",
-    version="0.1.0",
-    py_modules=["gofile"],
-    description="A Python wrapper for the GoFile API",
-    long_description=open("README.md", "r").read(),
-    long_description_content_type="text/markdown",
-    author="Codec04",
-    license="MIT",
-    packages=find_packages(),
-    project_urls={
-        "Documentation": "https://github.com/Codec04/gofile/wiki/",
-        "GitHub": "https://github.com/Codec04/gofile/"
-    },
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent"
-    ],
-    install_requires=["requests~=2.25.1"]
+from setuptools import setup, find_packages
+from gofile2 import __version__ as v
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+if os.path.isfile('requirements.txt'):
+  with open('requirements.txt') as req:
+    reques = req.read().splitlines()
+else:
+  reques = [
+    'requests'
+  ]
+
+if os.path.isfile('README.md'):
+  with open(('README.md'), encoding='utf-8') as readmeh:
+    big_description = readmeh.read()
+else:
+  big_description = "Gofile2 is an API wrapper for Gofile API"
+
+
+setup(name='gofile2',
+version=v,
+description='An API wrapper for Gofile API',
+url='https://github.com/Itz-fork/Gofile2',
+author='Itz-fork, Codec04',
+author_email='itz-fork@users.noreply.github.com',
+license='MIT',
+packages=find_packages(),
+download_url=f"https://github.com/Itz-fork/Gofile2/releases/tag/Gofile2-{v}",
+keywords=['Gofile', 'Api-wrapper', 'Gofile2'],
+long_description=big_description,
+long_description_content_type='text/markdown',
+install_requires=reques,
+classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Education',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.9',
+  ],
 )
